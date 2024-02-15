1e6cae0be009177965488108de798a24d191876b
Michael Bartlett <michael@ou.edu>
2024-02-15 10:01:27 -0600

    Added constraints to tables

--- create_database.sql
+++ create_database.sql
@@ -4,18 +4,22 @@ USE CS_Capstone;
 
 -- Create the users table
 CREATE TABLE IF NOT EXISTS users (
-	user_id int primary key,
-    username varchar(64) unique
+	user_id INT PRIMARY KEY,
+    username VARCHAR(64) UNIQUE NOT NULL
 );
 
 -- Create the items table
 CREATE TABLE IF NOT EXISTS items (
-	item_id int primary key,
-    seller_id int,
-    highest_bidder_id int,
-    item_title varchar(64),
-    date_of_listing datetime,
-    date_of_closing datetime,
-    item_description varchar(5000),
-    current_price int
+	item_id INT PRIMARY KEY,
+    seller_id INT NOT NULL,
+    highest_bidder_id INT,
+    item_title VARCHAR(64) NOT NULL,
+    date_of_listing DATETIME NOT NULL,
+    date_of_closing DATETIME NOT NULL,
+    item_description VARCHAR(5000),
+    current_price INT NOT NULL,
+    
+    -- Add the constraints to the table
+    FOREIGN KEY (seller_id) REFERENCES users(user_id),
+    FOREIGN KEY (highest_bidder_id) REFERENCES users(user_id)
 );
