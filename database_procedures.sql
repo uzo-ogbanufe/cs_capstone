2c0af1c98b3e9553c03271245112b386d8ff13d3
Michael Bartlett <michael@ou.edu>
2024-02-15 11:04:06 -0600

    Implemented AddItem procedure

--- database_procedures.sql
+++ database_procedures.sql
@@ -1,9 +1,9 @@
 USE CS_Capstone;
 
 -- Procedure to add a user to the database
-DROP PROCEDURE IF EXISTS add_user;
+DROP PROCEDURE IF EXISTS AddUser;
 DELIMITER //
-CREATE PROCEDURE add_user(
+CREATE PROCEDURE AddUser(
     IN new_username VARCHAR(64)
 )
 BEGIN
@@ -13,3 +13,26 @@ BEGIN
     INSERT IGNORE INTO users (username) VALUES (new_username);
 END //
 DELIMITER ;
+
+-- Procedure to add an item to the database
+DROP PROCEDURE IF EXISTS AddItem;
+DELIMITER //
+CREATE PROCEDURE AddItem(
+    IN new_title VARCHAR(64),
+    IN new_date_of_closing DATETIME,
+    IN new_current_price INT,
+    IN new_description VARCHAR(5000),
+    IN new_seller_id VARCHAR(64),
+    IN new_date_of_listing DATETIME
+)
+BEGIN
+    -- Add the item to the database
+    INSERT IGNORE INTO items (item_title, date_of_closing, current_price, item_description, seller_id, date_of_listing) 
+    VALUES (new_title, new_date_of_closing, new_current_price, new_description, new_seller_id, new_date_of_listing);
+    
+    -- Return true on success FIXME
+    SELECT TRUE;
+END //
+DELIMITER ;
+
+call AddItem('Game', '2020-03-01 12:00:00', 5000, 'Super Mario game', 3, '2020-01-01 00:00:00');
