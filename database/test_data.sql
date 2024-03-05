8385d6c3b5ccdc0d191e351a243fd9d77b612aa4
Michael Bartlett <michael@ou.edu>
2024-03-05 14:11:23 -0600

    Added test data

---
+++ database/test_data.sql
@@ -0,0 +1,14 @@
+USE CS_Capstone;
+
+-- Add test data to the user table
+INSERT INTO users (username) VALUES
+	('Alice'), 
+    ('Bob'),
+    ('Catherine'),
+    ('Daniel');
+    
+-- Add test data into the items table
+INSERT INTO items (seller_id, highest_bidder_id, item_title, date_of_listing, date_of_closing, item_description, current_price) VALUES
+    (1, 3, 'Shirt', '2020-01-01 00:00:00', '2020-02-01 00:00:00', 'A vintage T-Shirt', 4000),
+    (2, 4, 'Pants', '2020-01-01 00:00:00', '2020-02-01 00:00:00', 'A pair of used jeans', 6000),
+    (4, NULL, 'Socks', '2020-01-01 00:00:00', '2020-02-01 00:00:00', '10 pairs of pink socks', 1500);
