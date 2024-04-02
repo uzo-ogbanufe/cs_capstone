c1bef9deecea2ba1ce4275ffed6a9cffa74e78eb
Michael <michael@ou.edu>
2024-04-02 16:09:31 -0500

    Formatted price

--- auction_site/get_items/views.py
+++ auction_site/get_items/views.py
@@ -5,21 +5,33 @@ from datetime import datetime
 
 def get_items(request):
     '''
-    Retrieve all items from the database and send them to the
-    frontend
+    Retrieve all items from the database and send them to the frontend
     '''
     # Connect to the database
     try:
         with connection.cursor() as cursor:
             # Call the procedure to get all of the items
             now = datetime.now()
-            cursor.callproc('getAllItems', ['2019-01-01 00:00:00'])
+            cursor.callproc('getAllItems', [now])
 
             # Get all of the items from the query
             results = cursor.fetchall()
 
+            # Create a new array so we can format the data
+            items = []
+
+            # Format the price
+            PRICE_INDEX = 2
+            for item in results:
+                temp = []
+                for attribute in item:
+                    temp.append(attribute)
+                price = item[PRICE_INDEX] / 100
+                temp[PRICE_INDEX] = f"${price:.2f}"
+                items.append(temp)
+
             # Render the webpage
-            return render(request, 'get_items.html', {"items": results})
+            return render(request, 'get_items.html', {"items": items})
     
     # If an error occurs, display the error
     except Exception as e:
