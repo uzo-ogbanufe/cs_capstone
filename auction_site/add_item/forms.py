c2489300a35ef19258e4bede84decd7f6bdd066d
murmur6 <89601206+murmur6@users.noreply.github.com>
2024-04-09 14:02:31 -0500

    Update forms.py
    changed input size to 64

--- auction_site/add_item/forms.py
+++ auction_site/add_item/forms.py
@@ -1,11 +1,11 @@
 from django import forms
 
 class UserForm(forms.Form):
-    username = forms.CharField(label='Username', max_length=100)
+    username = forms.CharField(label='Username', max_length=64)
 
 class ItemForm(forms.Form):
-    username = forms.CharField(label='Username', max_length=100)
-    title = forms.CharField(label='Title', max_length=100)
+    username = forms.CharField(label='Username', max_length=64)
+    title = forms.CharField(label='Title', max_length=64)
     end_date = forms.DateField(label='End Date')
     initial_price = forms.DecimalField(label='Initial Price', max_digits=5, decimal_places=2)
     description = forms.CharField(label='Description', widget=forms.Textarea)
