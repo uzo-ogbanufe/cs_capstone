367262dd0a753f1b7efe8a335da23806f5388d64
Michael Bartlett <michael@ou.edu>
2024-04-09 15:02:37 -0500

    Fixed precision bug

--- auction_site/add_item/forms.py
+++ auction_site/add_item/forms.py
@@ -2,14 +2,6 @@ from django import forms
 from django.core.exceptions import ValidationError
 from django.utils.translation import gettext_lazy as _
 
-# Create validator that checks if a value is non-negative
-def non_negative(value):
-    if value < 0:
-        raise ValidationError(
-            _("Value must be positive"),
-            params={"value": value},
-        )
-
 class UserForm(forms.Form):
     username = forms.CharField(label='Username', max_length=64)
 
@@ -17,5 +9,5 @@ class ItemForm(forms.Form):
     username = forms.CharField(label='Username', max_length=64)
     title = forms.CharField(label='Title', max_length=64)
     end_date = forms.DateTimeField(label='End Date')
-    initial_price = forms.DecimalField(label='Initial Price', max_digits=8, decimal_places=2, validators=[non_negative])
+    initial_price = forms.DecimalField(label='Initial Price', max_digits=8, decimal_places=2, min_value=0)
     description = forms.CharField(label='Description', widget=forms.Textarea)
