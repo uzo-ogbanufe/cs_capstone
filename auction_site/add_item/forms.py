e1fc940434a1b48862a7340787ef096f5ce2974e
murmur6 <89601206+murmur6@users.noreply.github.com>
2024-04-09 13:44:11 -0500

    Update forms.py
    added a form to input username

--- auction_site/add_item/forms.py
+++ auction_site/add_item/forms.py
@@ -4,6 +4,7 @@ class UserForm(forms.Form):
     username = forms.CharField(label='Username', max_length=100)
 
 class ItemForm(forms.Form):
+    username = forms.CharField(label='Username', max_length=100)
     title = forms.CharField(label='Title', max_length=100)
     end_date = forms.DateField(label='End Date')
     initial_price = forms.DecimalField(label='Initial Price', max_digits=5, decimal_places=2)
