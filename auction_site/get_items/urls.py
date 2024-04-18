88b420160426d991111ef4322f593940cb82e99f
uzo.s.ogbanufe-1@ou.edu <uzoey101@gmail.com>
2024-04-18 15:21:00 -0500

    works and shows the new page

--- auction_site/get_items/urls.py
+++ auction_site/get_items/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
+from .views import get_items, item_detail  # Import your view function
+
 
 urlpatterns = [
     path('', views.get_items, name='get_items'),
+    path('item/<int:item_id>/', item_detail, name='item_detail'),
 ]
